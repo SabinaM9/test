9c68ae8daf0e1e7ffb764c01b0140a78e07ea2bc
Sabina Mussaeva <smussaeva@ozon.ru>
2022-04-12 18:20:43 +0300

    answers

--- Exercise_1.py
+++ Exercise_1.py
@@ -4,4 +4,11 @@ public class Oddity {
 }
 }
 
-# I suggest that this class
+# This function is created to check if the number is odd. It seems to me that the current logic is right.
+# Bot I would like to correct the style of this code, my version:
+
+public class Oddity {
+    public static boolean isOdd(int i) {
+        return i % 2 == 1;
+    }
+}
